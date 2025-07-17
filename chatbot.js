4e264eef3446b7e102cae107200dfb87b080b5ec
FeyoMx <elfeyo1980@gmail.com>
2025-07-16 23:32:00 -0600

    ngrok url

--- chatbot.js
+++ chatbot.js
@@ -129,7 +129,7 @@ function formatDisplayNumber(fullNumber) {
  */
 function sendToN8n(message) {
       // ¡IMPORTANTE! Verifica que esta URL sea la correcta en tu n8n
-  const n8nWebhookUrl = 'http://localhost:5678/webhook/58417d94-89dd-4915-897d-a2973327aade';
+  const n8nWebhookUrl = 'https://791a621e3cd9.ngrok-free.app/webhook/58417d94-89dd-4915-897d-a2973327aade';
 
   // Construimos un payload base con la información más relevante.
   // Enviamos el mensaje completo en 'rawMessage' para tener toda la data disponible en n8n.
